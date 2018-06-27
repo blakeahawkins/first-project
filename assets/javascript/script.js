54f1a642dcd03e85b0477c3639cf3bb43817a883
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-06-27 00:15:31 -0500

    linked files to html

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCLXyUhUnq6KT3IOsYNvWKYtnrGVXqZt08",
+    authDomain: "nrfpc-fdbbd.firebaseapp.com",
+    databaseURL: "https://nrfpc-fdbbd.firebaseio.com",
+    projectId: "nrfpc-fdbbd",
+    storageBucket: "nrfpc-fdbbd.appspot.com",
+    messagingSenderId: "455767813152"
+  };
+  firebase.initializeApp(config);
